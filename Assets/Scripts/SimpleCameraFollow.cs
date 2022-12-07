c5c2c2ce7db9bb6880631327d0f8438b8edef7b3
MedInf37 <64637@gmx.de>
2022-12-07 18:28:43 +0100

    Camera Follow simplified

---
+++ Assets/Scripts/SimpleCameraFollow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraFollow : MonoBehaviour
+{
+    public GameObject playerRef;
+    [SerializeField] float speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPosition = this.transform.position;
+        Vector3 playerPos = playerRef.transform.position;
+        playerPos.z = cameraPosition.z;
+        Vector3 directionToTarget = (playerPos - cameraPosition).normalized;
+        float currentDistance = Vector3.Distance(playerPos, cameraPosition);
+        if (currentDistance > 0)
+        {
+            Vector3 newCameraPosition = cameraPosition + speed * Time.deltaTime * directionToTarget * currentDistance;
+            float futureDistance = Vector3.Distance(playerPos, newCameraPosition);
+            if (futureDistance > currentDistance)
+            {
+                //Overshot the Target
+                newCameraPosition = playerPos;
+            }
+            this.transform.position = newCameraPosition;
+        }
+    }
+}
