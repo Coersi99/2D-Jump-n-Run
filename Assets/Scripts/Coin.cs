b79e9c4d6fe6326a8764a9de736945ce05fcb921
MedInf37 <64637@gmx.de>
2022-12-30 16:09:39 +0100

    Coin and Goal better

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -5,26 +5,13 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     [SerializeField] int coinValue = 10;
-    private GameObject CoinManagerRef;
     private bool collectable = true;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        CoinManagerRef = GameObject.FindGameObjectWithTag("CoinManager");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collectable)
         {
-            CoinManagerRef.GetComponent<CoinManager>().AddToScore(coinValue);
+            CoinManager.instance.AddToScore(coinValue);
             collectable = false;
         }
         Destroy(this.gameObject);
