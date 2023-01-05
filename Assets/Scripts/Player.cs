658afd0f8b946269a44e47025252c17c0025bc96
Coersi99 <cb.fleger@live.de>
2023-01-05 14:31:16 +0100

    Bug Fix: Player can move during pause

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -68,7 +68,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isDashing){
+        if(isDashing || PauseMenu.GameIsPaused){
             return;
         }
 
